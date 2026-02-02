cd83a95915840dfca7fc9217c79687a685594342
Muganagouda, Kavya - Contractor {PEP} <Kavya.Muganagouda.Contractor@pepsico.com>
2026-02-02 17:51:47 +0530

    notes

--- test.py
+++ test.py
@@ -1,8 +1,7 @@
-def my_func(*args):
-    """my doc on function"""
-    return sum(args)
-
-l = [1, 2, 3]
-print(my_func(*l))
-
-print(help(my_func))
+l1 = [1, 2, 3]
+l2 = [10, 20, 30]
+l3 = [100, 200, 300]
+def fact(n):
+    return 1 if n <= 1 else n * fact(n - 1)
+result = map(fact, l2)
+print(list(result))
